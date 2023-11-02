42eea4840c5de8504b2fa1e48393e88d94f08c60
mysterypaint <mysterypainto@gmail.com>
2023-11-02 00:02:52 -0700

    Cooldown timer (1 message buffer) added for bot replies

--- src/index.js
+++ src/index.js
@@ -48,6 +48,10 @@ var mifuTimer = getRandomInt(20, 200);
 var okeiTimer = getRandomInt(20, 300);
 var yuptuneTimer = getRandomInt(200, 2000);
 
+var ayylmaoCooldownTimer = 0;
+var maybenotCooldownTimer = 0;
+var sayoriCooldownTimer = 0;
+
 client.on('ready', () => console.log(`${client.user.tag} has logged in!`));
 
 /**
@@ -148,22 +152,41 @@ client.on('messageCreate', (message) => {
     if (yuptuneTimer <= 0) {
         let targChannel = message.client.channels.cache.get(ARK_CHANNEL_ID_ROOM3);
         sendLocalFile(targChannel, 'src/img/memes/Yuptune.gif', 'Yuptune.gif', 'Yuptune');
-        yuptuneTimer = getRandomInt(200, 2000);
+        yuptuneTimer = getRandomInt(200, 1000);
     }
 
     if (msgContent.includes('69')) {
         message.channel.send("nice");
     }
 
-    if (msgContent.toLowerCase() == 'ayy') {
-        message.channel.send("ayy lmao");
-    } else if (msgContent.toLowerCase() == 'maybe') {
-        message.channel.send("or maybe not");
-    } else if (message.author.id == CASTIE_ID) {
+    if (ayylmaoCooldownTimer <= 0) {
+        if (msgContent.toLowerCase() == 'ayy') {
+            message.channel.send("ayy lmao");
+            ayylmaoCooldownTimer = 1;
+        }
+    } else
+        ayylmaoCooldownTimer--;
+    
+    if (maybenotCooldownTimer <= 0) {
+        if (msgContent.toLowerCase() == 'maybe') {
+            message.channel.send("or maybe not");
+            maybenotCooldownTimer = 1;
+        }
+    } else
+        maybenotCooldownTimer--;
+
+    if (sayoriCooldownTimer <= 0) {
+        if (msgContent.toLowerCase().includes('sayori')) {
+            message.channel.send("(( sayori.jpg ))");
+            sayoriCooldownTimer = 1;
+        } 
+    } else
+        sayoriCooldownTimer--;
+    
+    if (message.author.id == CASTIE_ID) {
         let guildID = message.guild.id;
         let channelID = message.channel.id;
-
-        const allowed = (element) => element == channelID;
+        let allowed = (element) => element == channelID;
         
         
         if (guildID != ARK_GUILD_ID) {
