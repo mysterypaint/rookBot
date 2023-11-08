03c5675420de20275234463894b1a4021a446383
mysterypaint <mysterypainto@gmail.com>
2023-11-08 12:30:35 -0800

    Adjust attachment limit to 10; code beautification

--- src/SiteScraper.js
+++ src/SiteScraper.js
@@ -1,126 +1,127 @@
 class SiteScraper {
-  constructor() {
-  }
+	constructor() {}
 
-  /**
-   * Fetches and returns JSON data from the VXTwitter API
-   */
-  static async fetchTweetData(twitURL) {
-    try {
-      twitURL = twitURL.replace(/(https?:\/\/)?(www\.)?((vx)?twitter|x).com/i, "https://api.vxtwitter.com");
-      const response = await fetch(twitURL);
+	/**
+	 * Fetches and returns JSON data from the VXTwitter API
+	 */
+	static async fetchTweetData(twitURL) {
+		try {
+			twitURL = twitURL.replace(/(https?:\/\/)?(www\.)?((vx)?twitter|x).com/i, "https://api.vxtwitter.com");
+			const response = await fetch(twitURL);
 
-      if (!response.ok) {
-        console.log(`HTTP error: ${response.status}`);
-        return -2;
-      }
-      // after this line, our function will wait for the `response.json()` call to be settled
-      // the `response.json()` call will either return the parsed JSON object or throw an error
+			if (!response.ok) {
+				console.log(`HTTP error: ${response.status}`);
+				return -2;
+			}
+			// after this line, our function will wait for the `response.json()` call to be settled
+			// the `response.json()` call will either return the parsed JSON object or throw an error
 
-      const jsonDat = await response.json();
-      const jString = await JSON.stringify(jsonDat);
-      return jString;
-    } catch (error) {
-      console.log(`Could not get products: ${error}`);
-      return -1;
-    }
-  }
-  
-  static async fetchTweetCacheFromURL(inURL) {
-    try {
-        // Grab tweet data from the URL as a JSON object
-        const jString = await this.fetchTweetData(inURL);
+			const jsonDat = await response.json();
+			const jString = await JSON.stringify(jsonDat);
+			return jString;
+		} catch (error) {
+			console.log(`Could not get products: ${error}`);
+			return -1;
+		}
+	}
 
-        // after this line, our function will wait for the `response.json()` call to be settled
-        // the `response.json()` call will either return the parsed JSON object or throw an error
+	static async fetchTweetCacheFromURL(inURL) {
+		try {
+			// Grab tweet data from the URL as a JSON object
+			const jString = await this.fetchTweetData(inURL);
 
-        let tweetObj = await JSON.parse(jString);
+			// after this line, our function will wait for the `response.json()` call to be settled
+			// the `response.json()` call will either return the parsed JSON object or throw an error
 
-        // We want the tweeter's username, and all of the media URLs from the tweet
-        let mediaURLs = await tweetObj.mediaURLs;
-        let username = await "@" + tweetObj.user_screen_name;
-        let url = await tweetObj.tweetURL;
-        
-        return [mediaURLs, username, url];
-    } catch (error) {
-      /*
-      message.channel.send("Could not resolve to host :sob: :broken_heart:").catch((err) => {
-        console.log(err)
-      });*/
-      console.error(`Could not get products: ${error}`);
-    }
-  }
+			let tweetObj = await JSON.parse(jString);
 
-    
-  static scrapeURLs(inStr, website) {
-    let collectedURLs = [];
+			// We want the tweeter's username, and all of the media URLs from the tweet
+			let mediaURLs = await tweetObj.mediaURLs;
+			let username = await "@" + tweetObj.user_screen_name;
+			let url = await tweetObj.tweetURL;
 
-    let regex = /.+/gm;
+			return [mediaURLs, username, url];
+		} catch (error) {
+			/*
+			message.channel.send("Could not resolve to host :sob: :broken_heart:").catch((err) => {
+			  console.log(err)
+			});*/
+			console.error(`Could not get products: ${error}`);
+		}
+	}
 
-    inStr = inStr.replaceAll(' ', '\n');
-    inStr = inStr.replaceAll('https://', '');
-    inStr = inStr.replaceAll('http://', '');
-    regex = /(https?:\/\/(www\.)?)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)/gm;
 
-    try {
-      inStr.match(regex).forEach((element) => {
-        collectedURLs.push("https://" + element);
-      });
-    } catch (err) {
-      // No URLs were found; Return -1
-      return -1;
-    }
+	static scrapeURLs(inStr, website) {
+		let collectedURLs = [];
 
-    return collectedURLs;
-  }
+		let regex = /.+/gm;
 
-  static wrapURL(inStr, regex) {
-    if (regex == undefined)
-      regex = /(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/igm;
-    return inStr.replace(regex, function(url) {
-      return '<' + url.trim() + '>';
-    });
-  }
+		inStr = inStr.replaceAll(' ', '\n');
+		inStr = inStr.replaceAll('https://', '');
+		inStr = inStr.replaceAll('http://', '');
+		regex = /(https?:\/\/(www\.)?)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)/gm;
 
-  static wrapAllURLs(inStrArray) {
-    const regex = /(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z_0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z_0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z_0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z_0-9+&@#\/%=~_|$])/igm;
-    var urlCollection = "";
+		try {
+			inStr.match(regex).forEach((element) => {
+				collectedURLs.push("https://" + element);
+			});
+		} catch (err) {
+			// No URLs were found; Return -1
+			return -1;
+		}
 
-    try {
-      for (var i = 0; i < inStrArray.length; i++) {
-        var thisURL = inStrArray[i];
-        urlCollection += this.wrapURL(thisURL, regex);
+		return collectedURLs;
+	}
 
-        if (i < inStrArray.length - 1)
-          urlCollection += " | ";
-      }
-      return urlCollection;
-    } catch (err) {
-      console.log("Error wrapping string array: " + inStrArray);
-      return 'undefined';
-    }
-  }
+	static wrapURL(inStr, regex) {
+		if (regex == undefined)
+			regex = /(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/igm;
+		return inStr.replace(regex, function(url) {
+			return '<' + url.trim() + '>';
+		});
+	}
+
+	static wrapAllURLs(inStrArray) {
+		const regex = /(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z_0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z_0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z_0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z_0-9+&@#\/%=~_|$])/igm;
+		var urlCollection = "";
+
+		try {
+			for (var i = 0; i < inStrArray.length; i++) {
+				var thisURL = inStrArray[i];
+				urlCollection += this.wrapURL(thisURL, regex);
+
+				if (i < inStrArray.length - 1)
+					urlCollection += " | ";
+			}
+			return urlCollection;
+		} catch (err) {
+			console.log("Error wrapping string array: " + inStrArray);
+			return 'undefined';
+		}
+	}
 }
 
 // Static properties
 SiteScraper.sitesToParse = [];
 SiteScraper.collectedTwitSites = [];
 SiteScraper.atLeastOneValidURL = {
-  value: false,
-  get getVal() { return this.value; },
-  set changeVal(newVal) {
-    this.value = newVal;
-  }
+	value: false,
+	get getVal() {
+		return this.value;
+	},
+	set changeVal(newVal) {
+		this.value = newVal;
+	}
 }
 
 // Declare enums
 SiteScraper.Websites = {
-  Twitter: 0,
-  VxTwitter: 1,
-  Pixiv: 2,
-  Phixiv: 3,
-  Tiktok: 4,
-  VxTiktok: 5,
+	Twitter: 0,
+	VxTwitter: 1,
+	Pixiv: 2,
+	Phixiv: 3,
+	Tiktok: 4,
+	VxTiktok: 5,
 }
 
 export default SiteScraper;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import {
-  config
+	config
 } from 'dotenv';
 import {
-  Client,
-  Events,
-  GatewayIntentBits,
-  InteractionType,
-  Routes,
-  SlashCommandBuilder,
+	Client,
+	Events,
+	GatewayIntentBits,
+	InteractionType,
+	Routes,
+	SlashCommandBuilder,
 } from 'discord.js';
 import {
-  REST
+	REST
 } from '@discordjs/rest';
 import SayoriCommand from './commands/Sayori.js';
 import DiceRollCommand from './commands/DiceRoll.js';
@@ -23,11 +23,11 @@ import Rook from './Rook.js';
 
 // Create the client
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+	],
 });
 
 // Grab and declare all environment variables from .env
@@ -42,7 +42,7 @@ const GUILD_ID = process.env.GUILD_ID;
 const rook = new Rook(process);
 
 const rest = new REST({
-  version: '10'
+	version: '10'
 }).setToken(TOKEN);
 
 // Wait for the client to get ready; print to console when it logs in
@@ -52,32 +52,32 @@ client.once(Events.ClientReady, c => {
 
 /// Listens to every single Discord bot-slash-message sent to the server the bot is on (e.g. "/roll <value>")
 client.on(`interactionCreate`, (interaction) => {
-  rook.interactionCreateEvent(interaction);
+	rook.interactionCreateEvent(interaction);
 });
 
 /// Listens to every single Discord message sent to the server the bot is on
 client.on('messageCreate', async (message) => {
-  rook.messageCreateEvent(message);
+	rook.messageCreateEvent(message);
 });
 
 async function main() {
-  // Init all the Slash commands
-  const commands = [
-    SayoriCommand,
-    YuptuneCommand,
-    DiceRollCommand,
-    DiceRollListCommand,
-  ];
+	// Init all the Slash commands
+	const commands = [
+		SayoriCommand,
+		YuptuneCommand,
+		DiceRollCommand,
+		DiceRollListCommand,
+	];
 
-  try {
-    console.log('Started refreshing application (/) commands.');
-    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
-      body: commands,
-    });
-    client.login(TOKEN);
-  } catch (err) {
-    console.log(err);
-  }
+	try {
+		console.log('Started refreshing application (/) commands.');
+		await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+			body: commands,
+		});
+		client.login(TOKEN);
+	} catch (err) {
+		console.log(err);
+	}
 }
 
 main();
